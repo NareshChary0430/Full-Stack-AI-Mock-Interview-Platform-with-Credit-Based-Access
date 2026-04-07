6bb1830fd8083e16ebdd3667eb6b71b91d82eaf8
NareshChary0430 <nareshchari300@gmail.com>
2026-04-08 00:13:23 +0530

    created root server and also connected with the database

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import connectDB from './config/connectDB.js';
+
+
+const app = express();
+
+
+const PORT = process.env.PORT || 6000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
