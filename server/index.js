fe8a3a4cb72e7f30c6f08c3e24c03c7db09f4ff8
NareshChary0430 <nareshchari300@gmail.com>
2026-04-14 14:23:36 +0530

    creating jwt token for uses

--- server/index.js
+++ server/index.js
@@ -1,19 +1,31 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import connectDB from './config/connectDB.js';
-
-
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+
+import authRouter from './routes/auth.route.js'
+
+
+app.use('/api/auth', authRouter)
+
+
+
+
+
 
 
 const PORT = process.env.PORT || 6000;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
