cf72331b669af9751af4b97d164d1f850935e409
NareshChary0430 <nareshchari300@gmail.com>
2026-04-14 15:37:20 +0530

    implemented redux toolkit for state management

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,34 @@ import React from 'react'
 import { Routes,Route } from 'react-router-dom' 
 import Home from './pages/Home'
 import Auth from './pages/Auth'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { setUserData } from './redux/userSlice'
+import axios from 'axios'
+import { setUserData } from './redux/userSlice'
 
 
 export const ServerURL = "http://localhost:8000"
 
 const App = () => {
+
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    const getUser = async () => {
+      try {
+        const result = await axios.get(ServerURL + "/api/user/current-user", {withCredentials:true})
+        dispatch(setUserData(result.data))
+      } catch (error) {
+        console.log(error)
+        dispatch(setUserData(null))
+      }
+    }
+    getUser()
+
+  },[dispatch])
+
+
   return (
    <Routes> 
 
