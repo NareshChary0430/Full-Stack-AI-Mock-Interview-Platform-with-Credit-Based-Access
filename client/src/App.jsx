27e7448462126b416488e2428aa12c266bab9ad8
NareshChary0430 <nareshchari300@gmail.com>
2026-04-16 00:35:16 +0530

    implemented Navbar and AuthModel popup UI Components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,15 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom' 
 import Home from './pages/Home'
-import Auth from './pages/Auth'
+import Auth from './pages/auth.jsx'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { setUserData } from './redux/userSlice'
 import axios from 'axios'
-import { setUserData } from './redux/userSlice'
 
 
-export const ServerURL = "http://localhost:8000"
+
+export const ServerUrl = "http://localhost:8000"
 
 const App = () => {
 
@@ -19,13 +19,13 @@ const App = () => {
   useEffect(()=>{
     const getUser = async () => {
       try {
-        const result = await axios.get(ServerURL + "/api/user/current-user", {withCredentials:true})
+        const result = await axios.get(ServerUrl + "/api/user/current-user", {withCredentials:true})
         dispatch(setUserData(result.data))
       } catch (error) {
         console.log(error)
         dispatch(setUserData(null))
 
-        
+
       }
     }
     getUser()
